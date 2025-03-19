0d1879641065e15810bbe582dd235743bde770f7
Ashok <ashok.kk@jetwaystravels.com>
2025-03-19 12:58:08 +0530

    GDS COMMIT '19-03-2025'

--- ServiceLayer/obj/Debug/net6.0/ServiceLayer.AssemblyInfo.cs
+++ ServiceLayer/obj/Debug/net6.0/ServiceLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2bf6207d4fb9e6cc6ae1534cecf4d672b32507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac27ff7091451f2651a26e723a543c59e6681f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
